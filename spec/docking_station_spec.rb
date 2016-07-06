5551eb20400749849db21ac063e66d514c6546cf
Tommy Williams <tom@teow.co.uk>
2016-07-06 17:19:03 +0100

    12. Raising Expectations

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,22 @@
 require 'docking_station'
 
 describe DockingStation do
-  it {is_expected.to respond_to :release_bike}
-
+=begin
   it 'releases working bike' do
     bike = subject.release_bike
     expect(bike).to be_working
+  end
+=end
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error("No bikes available")
+    end
   end
 
     it {is_expected.to respond_to(:dock).with(1).argument}
