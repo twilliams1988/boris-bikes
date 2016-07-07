9c59c4944c2361c4a4aca3739500de9dcc0c55a4
Tommy Williams <tom@teow.co.uk>
2016-07-07 11:32:13 +0100

    14. Complex Attributes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,16 +2,20 @@ require_relative 'bike'
 
 class DockingStation
 
+  def initialize
+    @bikes = []
+  end
+
  def release_bike
-   fail "No bikes available" unless @bike
-   @bike
+   fail "No bikes available" if @bikes.empty?
+   @bikes.pop
  end
 
  def dock(bike)
-   fail "Docking station full" if @bike
-  @bike = bike
+   fail "Docking station full" if @bikes.count >= 20
+   @bikes << bike
  end
 
- attr_reader :bike
+ attr_reader :bikes
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -21,26 +21,28 @@ describe DockingStation do
 
    describe '#dock' do
 
-     it {is_expected.to respond_to(:dock).with(1).argument}
-
-     it 'docks something' do
-       bike = Bike.new
-       expect(subject.dock(bike)).to eq bike
-      end
-
      it 'raises an error when full' do
-       subject.dock(Bike.new)
+       20.times { subject.dock(Bike.new) }
        expect { subject.dock Bike.new }.to raise_error("Docking station full")
      end
 
    end
-
-    it {is_expected.to respond_to :bike}
+=begin
+    it {is_expected.to respond_to :bikes}
 
     it 'list bikes' do
       bike = Bike.new
       subject.dock(bike)
-      expect(subject.bike).to eq bike
+      expect(subject.bikes).to eq bike
     end
 
+
+    it {is_expected.to respond_to(:dock).with(1).argument}
+
+    it 'docks something' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+     end
+    
+=end
 end
